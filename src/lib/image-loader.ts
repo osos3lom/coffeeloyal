bad70b8189ea49413765d38f9563efdc923a232c
Osama 3alam <56493544+osos3lom@users.noreply.github.com>
2026-09-14 11:46:53 +0300

    Update image-loader.ts

--- src/lib/image-loader.ts
+++ src/lib/image-loader.ts
@@ -1,10 +1,8 @@
 export default function imageLoader({
   src,
-  width,
-  quality,
 }: {
   src: string;
-  width: number;
+  width?: number;
   quality?: number;
 }) {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
